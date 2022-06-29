8e1d3454474d8d7ae3ee0003b77da384bb104392
Vitor Stahelin <vitorjosestahelin@gmail.com>
2022-06-29 17:52:53 -0300

    feat: update flask test routes

--- src/tests/flask_server.py
+++ src/tests/flask_server.py
@@ -7,15 +7,36 @@ from stellar_error_handler import StellarErrorHandler
 app = Flask(__name__)
 
 
-@app.route("/")
-def error_tester():
+@app.route("/<int:sample_index>")
+def error_tester(sample_index):
+    """
+    Test the error handler with a sample envelope
+    """
     server = Server("https://horizon-testnet.stellar.org")
     try:
-        server.submit_transaction(ENVELOPE_SAMPLES["signature_error"])
+        server.submit_transaction(list(ENVELOPE_SAMPLES.values())[sample_index])
         return "Success?"
     except Exception as error:
         response = StellarErrorHandler(stellar_error=error.__dict__, main_net=False)
         return response.json()
+
+
+@app.route("/")
+def list_all_errors():
+    """
+    Test the error handler with all samples
+    """
+    server = Server("https://horizon-testnet.stellar.org")
+    response = {}
+    for sample in ENVELOPE_SAMPLES:
+        try:
+            server.submit_transaction(ENVELOPE_SAMPLES[sample])
+        except Exception as error:
+            response[sample] = StellarErrorHandler(
+                stellar_error=error.__dict__, main_net=False
+            ).json()
+
+    return response
 
 
 if __name__ == "__main__":
